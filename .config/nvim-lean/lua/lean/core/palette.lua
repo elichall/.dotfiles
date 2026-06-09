990dc115cd650da2e02b822a258d0a10632bb875
elichall <1elijah.hall@gmail.com>
2026-06-09 11:28:38 -0500

    lean nvim build

---
+++ .config/nvim-lean/lua/lean/core/palette.lua
@@ -0,0 +1,15 @@
+-- lua/lean/core/palette.lua
+-- This file is safely updated by your global engine, or serves as a static fallback.
+return {
+  bg       = "#0a1e24",
+  fg       = "#ecf0c1",
+  black    = "#6e5346",
+  red      = "#e35b00",
+  green    = "#5cab96",
+  yellow   = "#e3cd7b",
+  blue     = "#0f548b",
+  magenta  = "#e35b00",
+  cyan     = "#06afc7",
+  white    = "#f0f1ce",
+  gray     = "#684c31",
+}
